de733a11158c6315d96460659b00d2a4b0a20785
Rachel Fine <rachelefine@gmail.com>
2019-01-23 22:49:03 -0500

    removed files and old code

--- js/ARScene.js
+++ js/ARScene.js
@@ -2,13 +2,11 @@
 
 import React, { Component } from 'react';
 
-import {StyleSheet, Linking, TouchableHighlight} from 'react-native';
+import {StyleSheet, Linking} from 'react-native';
 
 import {
   ViroARScene,
-  ViroText,
   ViroConstants,
-  ViroFlexView,
   ViroBox,
   ViroMaterials
 } from 'react-viro';
@@ -22,7 +20,6 @@ export default class ARScene extends Component {
     this.state = {
       headerText : "Initializing AR...",
       subheaderText: "",
-      text: ""
     };
 
     // bind 'this' to functions
@@ -45,31 +42,6 @@ export default class ARScene extends Component {
           scale={[.5, .5, .5]}
           materials={["stone"]} 
           onClick={this._onClick}/>
-        {/*<ViroFlexView
-          style={{flexDirection: 'column', padding: .1}} 
-          width={2.0} height={3.5} 
-          position={[0, 0, -4]}
-          backgroundColor='rgba(0, 0, 0, 0.8)'
-          onClick={this._onClick}>
-        </ViroFlexView>
-                <ViroText 
-          text={this.state.headerText} 
-          position={[0, -1.85, -3]} 
-          style={styles.headerTextStyle} 
-          width={1.3} height={2}
-          onClick={this._onClick}/>
-        <ViroText 
-          text={this.state.subheaderText} 
-          position={[0, -2.15, -3]} 
-          style={styles.subheaderTextStyle} 
-          width={1.3} height={2}
-          onClick={this._onClick}/>
-        <ViroText 
-          text={this.state.text} 
-          position={[0, -2, -3]} 
-          style={styles.textStyle} 
-          width={1.3} height={2}
-        onClick={this._onClick}/>*/}
       </ViroARScene>
     );
   }
@@ -104,16 +76,6 @@ var styles = StyleSheet.create({
     color: 'black',
     textAlign: 'center',  
   },
-  textStyle: {
-    fontFamily: 'Arial',
-    fontSize: 12,
-    color: '#ffffff',
-    textAlign: 'center',  
-  },
-  touchable: {
-    height: 3,
-    width: 2
-  }
 });
 
 ViroMaterials.createMaterials({
